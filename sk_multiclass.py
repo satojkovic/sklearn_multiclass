39fdb2a835c75881fbac0a2c4e66c35d0c2efccf
satojkovic <satojkovic@gmail.com>
2016-04-23 18:29:21 +0900

    Plot resutls of three multiclass algorithms

--- sk_multiclass.py
+++ sk_multiclass.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from sklearn.datasets import load_iris
-from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
+from sklearn.multiclass import OneVsOneClassifier, \
+    OneVsRestClassifier, OutputCodeClassifier
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
@@ -14,27 +15,41 @@ def main():
     # We only take the first two features.
     iris = load_iris()
     X, y = iris.data[:, :2], iris.target
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    clf = OneVsRestClassifier(LinearSVC(random_state=0))
-    pred = clf.fit(X_train, y_train).predict(X_test)
-    y_uniq = map(np.str, np.unique(y_test))
-    print classification_report(y_test, pred,
-                                target_names=y_uniq)
 
-    # plot boundary
+    # multiclass and multilabel algorithms
+    ovr = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X, y)
+    ovo = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X, y)
+    oc = OutputCodeClassifier(LinearSVC(random_state=0)).fit(X, y)
+
+    # create a mesh to plot in
     h = .02
-    x_min, x_max = X_train[:, 0].min() - 1, X_train[:, 0].max() + 1
-    y_min, y_max = X_train[:, 1].min() - 1, X_train[:, 1].max() + 1
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
-    z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-    z = z.reshape(xx.shape)
-    plt.contourf(xx, yy, z, cmap=plt.cm.Paired, alpha=0.8)
 
-    # plot the training points
-    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=plt.cm.Paired)
-    plt.xlim(x_min, x_max)
-    plt.ylim(y_min, y_max)
+    # titles
+    titles = ['OneVsRest', 'OneVsOne', 'OutputCode']
+
+    # plot boundary
+    for idx, clf in enumerate((ovr, ovo, oc)):
+        # Prepare a canvas
+        plt.subplot(2, 2, idx + 1)
+        plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+        # plot boundary
+        z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        z = z.reshape(xx.shape)
+        plt.contourf(xx, yy, z, cmap=plt.cm.Paired, alpha=0.8)
+
+        # plot the training points
+        plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
+        plt.xlabel('Sepal length')
+        plt.ylabel('Sepal width')
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+        plt.title(titles[idx])
+
     plt.show()
 
 if __name__ == '__main__':
